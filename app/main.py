bc2c8e10c620c847bc8440c5830b7b1a1bfb72ce
Manezki <manezki@gmail.com>
2021-11-19 23:41:26 +0100

    Feat: MVP POST endpoint for comments

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-from flask import Flask
-
+from flask import Flask, request
 from transformers import pipeline
+
+from sentiment_scoring import process_comment, score_sentiment
 
 app= Flask(__name__)
 
@@ -12,7 +13,19 @@ classifier = pipeline(
                     framework="pt"
                     )
 
-@app.route('/')
+@app.route("/")
 def index():
   return "<h1>Junction 2021</h1>"
 
+
+@app.route("/comment", methods=["POST"])
+def receive_comment():
+  data = request.get_json()
+
+  user, comment = (data["user"], data["comment"])
+
+  social_credit_change = score_sentiment(process_comment(str(comment), classifier))
+
+  # Add comment & update the credit of the user
+
+  return ("", 200)
