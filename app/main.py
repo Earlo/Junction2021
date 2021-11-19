4de6fd7f4516bc7192b60e9f04c556f41c0cc9e6
Manezki <manezki@gmail.com>
2021-11-19 22:39:59 +0100

    Feat: Initial model for sentiment scoring

--- app/main.py
+++ app/main.py
@@ -1,5 +1,17 @@
 from flask import Flask
+
+from transformers import pipeline
+
 app= Flask(__name__)
+
+classifier = pipeline(
+                    "sentiment-analysis",
+                    model="distilbert-base-uncased-finetuned-sst-2-english",
+                    return_all_scores=True,
+                    function_to_apply="sigmoid",
+                    framework="pt"
+                    )
+
 @app.route('/')
 def index():
   return "<h1>Junction 2021</h1>"
