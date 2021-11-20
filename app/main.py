7e81c7d85bcfabfa0854b6a9c49a68e974eb5869
Manezki <manezki@gmail.com>
2021-11-20 13:41:18 +0100

    Feat: Freeze model weights during inference

--- app/main.py
+++ app/main.py
@@ -21,6 +21,9 @@ classifier = pipeline(
                     framework="pt"
                     )
 
+for param in classifier.model.base_model.parameters():
+    param.requires_grad = False
+
 login_manager.init_app(app)
 
 def loginAs(username):
